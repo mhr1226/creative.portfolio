74a0067050915d8a40eb5494fdf6655bd9f7ca44
mhr1226 <h.m.michiharu@gmail.com>
2024-05-02 14:06:00 +0900

    変数、定数の学習

--- 自作その2（デザインカンプ）/js/designCump.js
+++ 自作その2（デザインカンプ）/js/designCump.js
@@ -7,3 +7,27 @@ console.log(45%8);
 console.log('45+18');
 console.log('今日の天気は'+'晴れ');
 console.log('今日の天気は'+'曇り');
+
+let userName;
+
+userName='まひろ';
+
+var userNumber=80;
+
+console.log(userNumber);
+
+userName='はは';
+
+console.log(userName);
+
+const userPhone='iphone'
+
+console.log(userPhone);
+
+
+
+
+
+
+
+
