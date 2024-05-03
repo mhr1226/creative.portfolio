ac2a13e2f1b9557b2e91c7f6ecb459838f75b3f0
mhr1226 <h.m.michiharu@gmail.com>
2024-05-03 14:15:54 +0900

    if文とelse ifの練習と実践

--- 自作その2（デザインカンプ）/js/designCump.js
+++ 自作その2（デザインカンプ）/js/designCump.js
@@ -24,6 +24,28 @@ const userPhone='iphone'
 
 console.log(userPhone);
 
+const bigNumber=45
+
+const smallNumber=18
+
+if(bigNumber > smallNumber){
+  console.log('45は18より大きいです');
+}else{
+  console.log('これは間違いです');
+}
+
+let randomNumber=Math.floor(Math.random() * 5);
+
+console.log(randomNumber);
+
+if(randomNumber>1 && randomNumber<3){
+  console.log('大当たりです！');
+}else if(randomNumber===3 ||randomNumber===1){
+  console.log('惜しい！外れです');
+}else{
+  console.log('残念、外れです');
+}
+
 
 
 
