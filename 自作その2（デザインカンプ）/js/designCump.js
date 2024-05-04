3b240a9701769945f0ce4adaad470c79685bbab5
mhr1226 <h.m.michiharu@gmail.com>
2024-05-04 21:18:18 +0900

    for文とwhile文の学習

--- 自作その2（デザインカンプ）/js/designCump.js
+++ 自作その2（デザインカンプ）/js/designCump.js
@@ -46,6 +46,57 @@ if(randomNumber>1 && randomNumber<3){
   console.log('残念、外れです');
 }
 
+switch(randomNumber){
+  
+  case 1:
+    console.log('惜しい！外れです');
+    break;
+
+  case 2:
+    console.log('大当たりです！');
+    break;
+
+  case 3:
+    console.log('惜しい！少し外れです');
+    break;
+
+  default:
+    console.log('はずれー');
+    break;
+}
+
+for (let i = 50 ; i>=2; i-=5){
+  console.log(i);
+}
+
+const userFirstname=['前原','田中','高木','茂木','松丸'];
+
+userFirstname[5]='マイケル'
+
+console.log(userFirstname);
+
+console.log(68/(1.7*1.7));
+
+for(let i=0;i<userFirstname.length;i++){
+  console.log(userFirstname[i]);
+}
+
+
+let Number0 =31;
+
+if(Number0 %2 ===0 && Number0 %5 ===0){
+  console.log('2と5の倍数です');
+
+}else if(Number0 %3 === 0){
+  console.log('3の倍数です');
+}else{
+  console.log(Number0);
+}
+
+
+
+
+
 
 
 
