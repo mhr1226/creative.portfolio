434308ca3c75b1a984bf06939eafb3e2bb842c64
mhr1226 <h.m.michiharu@gmail.com>
2024-06-27 22:53:24 +0900

    ハンバーガーメニューの完成（微調整まだ）

--- 自作その６/js/script.js
+++ 自作その６/js/script.js
@@ -1,22 +1,24 @@
 // // ナビゲーションのクリック時の関数まとめ
-const animateSet = () =>{
-  $(() =>{
-    // 背景の表示
-  $('.nav-back').css('display','block');
-  $('nav').css('display','flex');
-   // バツ印の作成
-  $('.menu-bar div').css({
-    'border':'1px solid white',
-    'top':'50%'});
-  $('.menu-bar div:nth-of-type(2)').css({
-    'bottom':'auto',
-    'transform':'rotate(-45deg)'});
-  ;
-  // バツ印にアニメーションをつける
-  $('.menu-bar div:nth-of-type(1)').css('transform','rotate(45deg)');
-});
-}
+// const animateSet = () =>{
+//   $(() =>{
+//     // 背景の表示
+//   $('.nav-back,nav').css('display','flex');
+//    // バツ印の作成
+//   $('.menu-bar div').css({
+//     'border':'1px solid white',
+//     'top':'50%'});
+//   $('.menu-bar div:nth-of-type(2)').css({
+//     'bottom':'auto',
+//     'transform':'rotate(-45deg)'});
+//   ;
+//   // バツ印にアニメーションをつける
+//   $('.menu-bar div:nth-of-type(1)').css('transform','rotate(45deg)');
+// });
+// }
 
 $('.menu-bar').on('click',() =>{
-  animateSet();
+  $('nav,.nav-back').toggleClass('nav-open');
+  $('.menu-bar div').toggleClass('nav-close');
+  $('.menu-bar div:nth-of-type(1)').toggleClass('nav-close-1');
+  $('.menu-bar div:nth-of-type(2)').toggleClass('nav-close-2');
 });
